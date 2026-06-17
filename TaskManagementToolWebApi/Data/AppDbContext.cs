e564443cacd5b5c69ea57311ad968c4baecb702c
Shrijan <sshrijan65@gmail.com>
2026-06-17 09:21:35 +0545

    AppDbContext Setup

---
+++ TaskManagementToolWebApi/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagementToolWebApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
--- TaskManagementToolWebApi/Program.cs
+++ TaskManagementToolWebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagementToolWebApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -5,6 +8,11 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
